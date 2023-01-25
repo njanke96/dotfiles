68896918474df29420f6699880d2bc64db532842
Nathan Janke <nathanjanke96@gmail.com>
2023-01-25 12:14:22 -0700

    updated fish config

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -2,9 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+source $HOME/.config/fish/secrets.fish
+
 set -U fish_prompt_pwd_dir_length 0
-set -gx DWMQTT_USERNAME Dworx_MQTT
-set -gx DWMQTT_PASSWORD X0Ij60y3dP9j
 set -gx PATH "/home/nathan/.local/bin" $PATH
 
 function fish_greeting
