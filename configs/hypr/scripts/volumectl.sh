e0580f7e38c1f20f19f7196be8278682f832d6dc
Nathan Janke <nathanjanke96@gmail.com>
2023-02-20 18:01:32 -0700

    more media keyss

--- configs/hypr/scripts/volumectl.sh
+++ configs/hypr/scripts/volumectl.sh
@@ -7,6 +7,7 @@ if [[ "$1" == "up" ]]; then
   if [ $VOLUME -lt 100 ]; then
     # increment the VOLUME by 5%
     pactl set-sink-volume @DEFAULT_SINK@ +5%
+    paplay $HOME/sounds/pop.ogg
   fi
 elif [[ "$1" == "down" ]]; then
   # get the default sink volume
@@ -15,6 +16,7 @@ elif [[ "$1" == "down" ]]; then
   if [ $VOLUME -gt 0 ]; then
     # decrement the VOLUME by 5%
     pactl set-sink-volume @DEFAULT_SINK@ -5%
+    paplay $HOME/sounds/pop.ogg
   fi
 else
   echo "Invalid argument. Please use 'up' or 'down'."
