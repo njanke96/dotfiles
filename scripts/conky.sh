ad53e5e71bd1b1b797d809fbfe66673b71f58001
Nathan Janke <nathanjanke96@gmail.com>
2025-11-26 19:01:50 -0800

    [linux] fix saii script

--- scripts/conky.sh
+++ scripts/conky.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-while true
-do
-  /home/nathan/AppImages/conky-ubuntu-24.04-x86_64-v1.22.2.AppImage
-  sleep 1
-done
+/home/nathan/AppImages/conky-ubuntu-24.04-x86_64-v1.22.2.AppImage
--- scripts/saii.sh
+++ scripts/saii.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-while true
-do
-  sway-audio-idle-inhibit
+until sway-audio-idle-inhibit; do
+  if [[ $(pgrep -f sway-audio-idle-inhibit) != 0 ]]; then
+    echo "sway-audio-idle-inhibit is running, exiting saii.sh" >&2
+    exit 0
+  fi
+  echo "sway-audio-idle-inhibit exited with code $? and will respawn" >&2
   sleep 1
-done
+done 
