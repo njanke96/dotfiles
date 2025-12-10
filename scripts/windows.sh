8c166464a6c767637395948cf36d3a3672629f69
Nathan Janke <nathanjanke96@gmail.com>
2025-12-09 21:27:59 -0800

    [linux] Add windows to labwc menu

--- scripts/windows.sh
+++ scripts/windows.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 #
 
+# ask for confirmation
+zenity --question --title="Reboot to Windows?" --text="Are you sure you want to leave Glorious Void?"
+
+response=$?
+
+if ! [ $response -eq 0 ]; then
+  exit 0
+fi
+
 # set default to windows
 sudo efibootmgr -n 0000
 
