aad655a0012fe63c5f5a86f1059c43793dc70f4c
Nathan Janke <nathanjanke96@gmail.com>
2026-05-10 14:23:04 -0700

    [linux] scripts: overlay layer for labnag script

--- scripts/nag.sh
+++ scripts/nag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-labnag -t 2 --background-color 282828ee --border-bottom-color d65d0e -m "$@" &
+labnag -t 2 --layer overlay --background-color 282828ee --border-bottom-color d65d0e -m "$@" &
 sleep 2
 
 # timeout not working?
