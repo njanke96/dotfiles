cb4ff5755eb7e56f83ea90e5e96a8cd912d27bdf
Nathan Janke <nate@codejank.dev>
2026-09-22 22:03:04 -0700

    [linux] swap to doas

--- scripts/windows.sh
+++ scripts/windows.sh
@@ -11,10 +11,10 @@ if ! [ $response -eq 0 ]; then
 fi
 
 # find the id of windows
-windows_id=$(sudo efibootmgr | grep -i "Windows Boot" | sed -e 's/\s.*$//' -e 's/^Boot//' -e 's/\*$//' | tail -n 1)
+windows_id=$(doas efibootmgr | grep -i "Windows Boot" | sed -e 's/\s.*$//' -e 's/^Boot//' -e 's/\*$//' | tail -n 1)
 
 # set default to windows
-sudo efibootmgr -n "$windows_id"
+doas efibootmgr -n "$windows_id"
 
 # reboot
 loginctl reboot
